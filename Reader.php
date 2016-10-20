c764422d2a0e4685fe8f229853eeee4b8e0cec3c
Ronnie <rix17172@gmail.com>
2016-10-20 10:44:08 +0200

    First preiw

---
+++ Reader.php
@@ -0,0 +1,73 @@
+<?php
+namespace Reader;
+
+class Reader{
+
+  const LF_LINETERMINATOR = 10;
+  const CR_LINETERMINATOR = 13;
+
+  const TAB_WHITESPACE = 9;
+  const VT_WHITESPACE  = 11;
+  const FF_WHITESPACE  = 12;
+  const SP_WHITESPACE  = 32;
+
+  private $peek = null;
+  private $index = -1;
+  private $code;
+
+  public function __construct(string $code){
+    $this->code = $code;
+  }
+
+  public function peek(){
+    if($this->peek != null)
+      return $this->peek;
+
+    return $this->get($this->index+1);
+  }
+
+  public function current(){
+    if($this->index == -1){
+      $this->index++;
+    }
+
+    return $this->eof() ? null : $this->code[$this->index];
+  }
+
+  public function next(){
+    $this->index++;
+    return $this->eof() ? null : $this->code[$this->index];
+  }
+
+  public function pop(){
+    if($this->index == -1){
+      $this->index++;
+    }
+
+    $buffer = $this->eof() ? null : $this->code[$this->index];
+    $this->index++;
+    return $buffer;
+  }
+
+  public function eof(){
+    return strlen($this->code)-1 < $this->index;
+  }
+
+  public function isLineTerminator(){
+    $ansii = ord($this->current());
+    return $ansii == self::LF_LINETERMINATOR || $ansii == self::CR_LINETERMINATOR;
+  }
+
+  public function isWhiteSpace(){
+    $ansii = ord($this->current());
+    return $ansii == self::TAB_WHITESPACE || $ansii == self::VT_WHITESPACE || $ansii == self::FF_WHITESPACE || $ansii == self::SP_WHITESPACE;
+  }
+
+  private function get(int $index){
+    if(strlen($this->code)-1 < $index){
+       return null;
+    }
+
+    return $this->code[$index];
+  }
+}
---
+++ Stack.php
@@ -0,0 +1,26 @@
+<?php
+namespace Stack;
+
+class Stack{
+  private $_stack = [];
+
+  public function size() : int{
+    return count($this->_stack);
+  }
+
+  public function get(int $i){
+    return $this->_stack[$i];
+  }
+
+  public function pop(){
+    return array_pop($this->_stack);
+  }
+
+  public function peek(){
+    return $this->_stack[count($this->_stack)-1];
+  }
+
+  public function push($context){
+    $this->_stack[] = $context;
+  }
+}
