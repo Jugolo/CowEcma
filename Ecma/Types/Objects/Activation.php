b21551e5e6833719e5f643cf2fa17a39a692f46f
Ronnie <rix17172@gmail.com>
2016-11-27 23:09:24 +0100

    Update Activation.php

--- Ecma/Types/Objects/Activation.php
+++ Ecma/Types/Objects/Activation.php
@@ -5,14 +5,15 @@ use Ecma\Types\Objects\HeadObject\HeadObject;
 use Ecma\Types\Objects\AgumentObject\AgumentObject;
 use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Value\Value;
+use Ecma\Ecma\Ecma;
 
 class Activation extends HeadObject{
-  public function __construct(HeadObject $obj, array $definedArg, array $callArg){
+  public function __construct(Ecma $ecma, HeadObject $obj, array $definedArg, array $callArg){
    if($obj->HasProperty("arguments"))
     $argument = $obj->Get("arguments");
    else
     $argument = new AgumentObject($obj, $this, $definedArg, $callArg);
 
-   $this->Put("arguments", new Property(new Value("Object", $argument), ["DontDelete"]));
+   $this->Put("arguments", new Property(new Value($ecma, "Object", $argument), ["DontDelete"]));
   }
 }
