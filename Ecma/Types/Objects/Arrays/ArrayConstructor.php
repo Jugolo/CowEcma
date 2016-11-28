c9ae037c0088c48eae615e6e3d23c3cc0bb42529
Ronnie <rix17172@gmail.com>
2016-11-28 12:25:18 +0100

    [New]Append ecma to value

--- Ecma/Types/Objects/Arrays/ArrayConstructor.php
+++ Ecma/Types/Objects/Arrays/ArrayConstructor.php
@@ -8,14 +8,18 @@ use Ecma\Types\Value\Value;
 use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Objects\Arrays\ArrayPrototype\ArrayPrototype;
 use Ecma\Types\Objects\Arrays\ArrayInstance\ArrayInstance;
+use Ecma\Ecma\Ecma;
 
 class ArrayConstructor extends HeadObject implements Call, Constructor{
   private $proto;
-  public function __construct(){
-    $this->proto = new ArrayPrototype($this);
+  protected $ecma;
+
+  public function __construct(Ecma $ecma){
+    $this->proto = new ArrayPrototype($this, $ecma);
+    $this->ecma = $ecma;
   }
-  public function call($obj, array $args) : Value{
-    $this->proto = new ArrayPrototype($this);
+  public function Call(Value $obj, array $args) : Value{
+    return $this->Construct($args);
   }
 
   public function Construct(array $args) : Value{
@@ -23,8 +27,8 @@ class ArrayConstructor extends HeadObject implements Call, Constructor{
     $array->Prototype = $this->proto;
     $array->Class = "Array";
     if(count($args) >= 2){
-      $array->length = new Value("Number", count($args));
-      $array->Put("length", new Property(new Value("Number", count($args))));
+      $array->length = new Value($this->ecma, "Number", count($args));
+      $array->Put("length", new Property(new Value($this->ecma, "Number", count($args))));
       for($i=0;$i<count($args);$i++)
         $array->Put($i, new Property($args[$i]));
     }elseif(count($args) == 1){
@@ -32,15 +36,15 @@ class ArrayConstructor extends HeadObject implements Call, Constructor{
         $array->length = $args[0];
         $array->Put("length", new Property($args[0]));
       }else{
-        $array->length = new Value("Number", 1);
-        $array->Put("length", new Property(new Value("Number", 1)));
+        $array->length = new Value($this->ecma, "Number", 1);
+        $array->Put("length", new Property(new Value($this->ecma, "Number", 1)));
         $array->Put("0", new Property($args[0]));
       }
     }else{
-      $array->length = new Value("Number", 0);
-      $array->Put("length", new Property(new Value("Number", 0)));
+      $array->length = new Value($this->ecma, "Number", 0);
+      $array->Put("length", new Property(new Value($this->ecma, "Number", 0)));
     }
-    $array->Put("prototype", new Property(new Value("Object", $this->proto), ["DontEnum", "DontDelete", "ReadOnly"]));
-    return new Value("Object", $array);
+    $array->Put("prototype", new Property(new Value($this->ecma, "Object", $this->proto), ["DontEnum", "DontDelete", "ReadOnly"]));
+    return new Value($this->ecma, "Object", $array);
   }
 }
