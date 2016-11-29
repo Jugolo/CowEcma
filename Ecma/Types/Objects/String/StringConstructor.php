f57307dd1eb02155997164152e167055d755cade
Ronnie <rix17172@gmail.com>
2016-11-29 10:47:46 +0100

    Update StringConstructor.php

--- Ecma/Types/Objects/String/StringConstructor.php
+++ Ecma/Types/Objects/String/StringConstructor.php
@@ -2,10 +2,21 @@
 namespace Ecma\Types\Objects\String\StringConstructor;
 
 use Ecma\Ecma\Ecma;
+use Ecma\Types\Value\Value;
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Types\Objects\Call\Call;
 
 class StringConstructor extends HeadObject{
+  protected $ecma;
+  
   public function __construct(Ecma $ecma){
+    $this->ecma = $ecma;
     $ecma->str = new StringPrototype($ecma);
+  }
+  
+  public function Call(Value $obj, array $arg) : Value{
+    if(count($arg) == 0)
+      return new Value($this->ecma, "String", "");
+    return new Value($this->ecma, "String", $arg[0]->ToString());
   }
 }
