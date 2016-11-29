eebdf360d7a58d53885118190916782618b4016d
Ronnie <rix17172@gmail.com>
2016-11-29 12:32:19 +0100

    Update StringPrototype.php

--- Ecma/Types/Objects/String/StringPrototype.php
+++ Ecma/Types/Objects/String/StringPrototype.php
@@ -5,10 +5,22 @@ use Ecma\Ecma\Ecma;
 use Ecma\Types\Value\Value;
 use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Types\Objects\Call\Call;
 use Ecma\Types\Objects\String\StringConstructor\StringConstructor;
 
 class StringPrototype extends HeadObject{
   public function __construct(StringConstructor $constructor, Ecma $ecma){
     $this->Put("constructor", new Property(new Value($ecma, "Object", $constructor)));
+    $this->Put("toString", new Property(new Value($ecma, "Object", new StringToString())));
+  }
+}
+
+class StringToString extends HeadObject implements Call{
+  public function Call(Value $obj, array $arg) : Value{
+    $o = $obj->ToObject();
+    if(!($o instanceof StringInstance)){
+      throw new \RuntimeException("toString should be method of StringInstance!!");
+    }
+    return new Value($obj->ecma, "String", $o->Value);
   }
 }
