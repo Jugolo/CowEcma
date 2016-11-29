060bfe4fb2b2220c8d04676f5c53728de87ac972
Ronnie <rix17172@gmail.com>
2016-11-29 12:25:49 +0100

    Update StringPrototype.php

--- Ecma/Types/Objects/String/StringPrototype.php
+++ Ecma/Types/Objects/String/StringPrototype.php
@@ -1,8 +1,14 @@
 <?php
 namespace Ecma\Types\Objects\String\StringPrototype\StringPrototype;
 
+use Ecma\Ecma\Ecma;
+use Ecma\Types\Value\Value;
+use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Types\Objects\String\StringConstructor\StringConstructor;
 
 class StringPrototype extends HeadObject{
-
+  public function __construct(StringConstructor $constructor, Ecma $ecma){
+    $this->Put("constructor", new Property(new Value($ecma, "Object", $constructor)));
+  }
 }
