d7efba7f693a9f5768439e7aaf7f937a7d61c61e
Ronnie <rix17172@gmail.com>
2016-11-29 10:26:15 +0100

    Update StringConstructor.php

--- Ecma/Types/Objects/String/StringConstructor.php
+++ Ecma/Types/Objects/String/StringConstructor.php
@@ -2,6 +2,7 @@
 namespace Ecma\Types\Objects\String\StringConstructor;
 
 use Ecma\Ecma\Ecma;
+use Ecma\Types\Objects\HeadObject\HeadObject;
 
 class StringConstructor extends HeadObject{
   public function __construct(Ecma $ecma){
