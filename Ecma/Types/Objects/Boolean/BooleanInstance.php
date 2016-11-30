22b95903395b287af9e3e4fb66a2fe18b327c33b
Ronnie <rix17172@gmail.com>
2016-11-30 19:23:20 +0100

    Update BooleanInstance.php

--- Ecma/Types/Objects/Boolean/BooleanInstance.php
+++ Ecma/Types/Objects/Boolean/BooleanInstance.php
@@ -1,6 +1,11 @@
 <?php
 namespace Ecma\Types\Objects\Boolean\BooleanInstance;
 
+use Ecma\Ecma\Ecma;
+use Ecma\Types\Value\Value;
+use Ecma\Types\Objects\Property\Property;
+use Ecma\Types\Objects\HeadObject\HeadObject;
+
 class BooleanInstance extends HeadObject{
   protected $ecma;
   public function __construct(Ecma $ecma, bool $bool){
