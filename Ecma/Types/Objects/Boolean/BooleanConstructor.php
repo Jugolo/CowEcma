d5d6b06f17116c7333341a1d550fd86e77b5000c
Ronnie <rix17172@gmail.com>
2016-11-30 12:55:07 +0100

    Update BooleanConstructor.php

--- Ecma/Types/Objects/Boolean/BooleanConstructor.php
+++ Ecma/Types/Objects/Boolean/BooleanConstructor.php
@@ -4,7 +4,12 @@ namespace Ecma\Types\Objects\Boolean\BooleanConstructor;
 use Ecma\Types\Objects\Call\Call;
 use Ecma\Types\Objects\Constructor;
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Ecma\Ecma;
 
 class BooleanConstructor extends HeadObject implements Call, Constructor{
-
+  protected $ecma;
+  
+  public function __construct(Ecma $ecma){
+    $this->ecma = $ecma;
+  }
 }
