355638b6b44e36daa3de1b2d312a6f8269e8cdf5
Ronnie <rix17172@gmail.com>
2016-11-30 14:57:54 +0100

    Update BooleanConstructor.php

--- Ecma/Types/Objects/Boolean/BooleanConstructor.php
+++ Ecma/Types/Objects/Boolean/BooleanConstructor.php
@@ -6,12 +6,16 @@ use Ecma\Types\Objects\Constructor;
 use Ecma\Types\Objects\HeadObject\HeadObject;
 use Ecma\Types\Value\Value;
 use Ecma\Ecma\Ecma;
+use Ecma\Types\Objects\Boolean\BooleanPrototype\BooleanPrototype;
+use Ecma\Types\Objects\Property\Property;
 
 class BooleanConstructor extends HeadObject implements Call, Constructor{
   protected $ecma;
   
   public function __construct(Ecma $ecma){
     $this->ecma = $ecma;
+    $ecma->_boolean = new BooleanPrototype($ecma);
+    $this->Put("prototype", new Property($ecma->_boolean));
   }
   
   public function Call(Value $obj, array $arg) : Value{
