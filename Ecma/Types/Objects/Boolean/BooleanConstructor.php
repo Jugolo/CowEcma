7672d632e1984c9ec2696e5075058e79d9f7b80c
Ronnie <rix17172@gmail.com>
2016-11-30 12:40:57 +0100

    Create BooleanConstructor.php

---
+++ Ecma/Types/Objects/Boolean/BooleanConstructor.php
@@ -0,0 +1,10 @@
+<?php
+namespace Ecma\Types\Objects\Boolean\BooleanConstructor;
+
+use Ecma\Types\Objects\Call\Call;
+use Ecma\Types\Objects\Constructor;
+use Ecma\Types\Objects\HeadObject\HeadObject;
+
+class BooleanConstructor extends HeadObject implements Call Constructor{
+
+}
