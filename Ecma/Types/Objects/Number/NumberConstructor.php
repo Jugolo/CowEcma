0756fc885fd21044f4360810e81cf107d0e25008
Ronnie <rix17172@gmail.com>
2016-12-04 19:40:13 +0100

    Update NumberConstructor.php

--- Ecma/Types/Objects/Number/NumberConstructor.php
+++ Ecma/Types/Objects/Number/NumberConstructor.php
@@ -4,14 +4,19 @@ namespace Ecma\Types\Objects\Number\NumberConstructor;
 use Ecma\Types\Objects\HeadObject\HeadObject;
 use Ecma\Types\Objects\Call\Call;
 use Ecma\Types\Objects\Constructor\Constructor;
+use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Value\Value;
 use Ecma\Ecma\Ecma;
+use Ecma\Types\Objects\Number\NumberPrototype\NumberPrototype;
 
 class NumberConstructor extends HeadObject implements Call, Constructor{
   protected $ecma;
   
   public function __construct(Ecma $ecma){
+    $ecma->number = new NumberPrototype($ecma);
     $this->ecma = $ecma;
+    
+    $this->Put("prototype", new Property(new Value($ecma, "Object", $ecma->number)));
   }
   
   public function Call(Value $value, array $arg) : Value{
