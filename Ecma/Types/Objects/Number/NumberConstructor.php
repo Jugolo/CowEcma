d1e431f902ef5e8071aa405238899499928a9bec
Ronnie <rix17172@gmail.com>
2016-12-04 19:24:35 +0100

    Update NumberConstructor.php

--- Ecma/Types/Objects/Number/NumberConstructor.php
+++ Ecma/Types/Objects/Number/NumberConstructor.php
@@ -2,7 +2,12 @@
 namespace Ecma\Types\Objects\Number\NumberConstructor;
 
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Ecma\Ecma;
 
 class NumberConstructor extends HeadObject{
+  protected $ecma;
   
+  public function __construct(Ecma $ecma){
+    $this->ecma = $ecma;
+  }
 }
