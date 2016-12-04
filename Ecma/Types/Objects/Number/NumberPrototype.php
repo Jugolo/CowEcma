60a5da1fbb933c7114f902e9ade499d8bf689dab
Ronnie <rix17172@gmail.com>
2016-12-04 23:31:24 +0100

    Update NumberPrototype.php

--- Ecma/Types/Objects/Number/NumberPrototype.php
+++ Ecma/Types/Objects/Number/NumberPrototype.php
@@ -18,14 +18,21 @@ class NumberPrototype extends HeadObject{
     
     $this->Put("constructor", new Property(new Value($ecma, "Object", $constructor)));
     $this->Put("toString", new Property(new Value($ecma, "Object", new NumberToString())));
+    $this->Put("valueOf", new Property(new Value($ecma, "Object", new NumberValueOf())));
+  }
+}
+
+class NumberValueOf extends HeadObject implements Call{
+  public function Call(Value $obj, array $arg) : Value{
+    return new Value($obj->value, "Number", $obj->ToNumber());
   }
 }
 
 class NumberToString extends HeadObject implements Call{
   public function Call(Value $obj, array $arg) : Value{
-    if(count($arg) == 0 || $arg->ToNumber() == 10)
+    if(count($arg) == 0 || $arg[0]->ToNumber() == 10)
       return new Value($obj->ecma, "String", $obj->ToString());
     
-    
+    return new Value($obj->ecma, "String", base_convert($arg[0]->ToNumber());
   }
 }
