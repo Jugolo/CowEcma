fe2f5962840fd514b6c88ec7e1d7b8afa38b7ddb
Ronnie <rix17172@gmail.com>
2016-12-05 13:23:46 +0100

    Update NumberInstance.php

--- Ecma/Types/Objects/Number/NumberInstance.php
+++ Ecma/Types/Objects/Number/NumberInstance.php
@@ -2,7 +2,15 @@
 namespace Ecma\Types\Objects\Number\NumberInstance;
 
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Types\Objects\Property\Property;
+use Ecma\Ecma\Ecma;
 
 class NumberInstance extends HeadObject{
-
+  protected $ecma;
+  
+  public function __construct(Ecma $ecma, int $number){
+    $this->ecma = $ecma;
+    $this->Value = $number;
+    $this->Put("prototype", new Property($ecma->number));
+  }
 }
