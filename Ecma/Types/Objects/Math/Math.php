2a8fdabc2ff51d40ba6ffca943002e0e61d7c039
Ronnie <rix17172@gmail.com>
2016-12-05 14:05:33 +0100

    Update Math.php

--- Ecma/Types/Objects/Math/Math.php
+++ Ecma/Types/Objects/Math/Math.php
@@ -4,6 +4,7 @@ namespace Ecma\Types\Objects\Math\Math;
 use Ecma\Ecma\Ecma;
 use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Types\Objects\Call\Call;
 use Ecma\Types\Value\Value;
 
 class Math extends HeadObject{
@@ -11,5 +12,15 @@ class Math extends HeadObject{
   
   public function __construct(Ecma $ecma){
     $this->ecma = $ecma;
+    $this->Put("abs", new Property(new Value($ecma, "Object", new MathAbs())));
+  }
+}
+
+class MathAbs extends HeadObject implements Call{
+  public function Call(Value $obj, array $arg) : Value{
+    $number = $arg[0]->ToNumber();
+    if(is_nan($number) || $number == 0)
+      return $arg[0];
+    return new Value($obj->ecma, "Number", +$number);
   }
 }
