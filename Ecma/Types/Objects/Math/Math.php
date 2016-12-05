990033b5becbb312e6ad50edf3b21cfeb1957560
Ronnie <rix17172@gmail.com>
2016-12-05 14:33:10 +0100

    Update Math.php

--- Ecma/Types/Objects/Math/Math.php
+++ Ecma/Types/Objects/Math/Math.php
@@ -14,6 +14,23 @@ class Math extends HeadObject{
     $this->ecma = $ecma;
     $this->Put("abs", new Property(new Value($ecma, "Object", new MathAbs())));
     $this->Put("acos", new Property(new Value($ecma, "Object", new MathAcos())));
+    $this->Put("asin", new Property(new Value($ecma, "Object", new MathAsin())));
+  }
+}
+
+class MathAsin extends HeadObject implements Call{
+  public function Call(Value $obj, array $arg) : Value{
+    $number = $arg[0]->ToNumber();
+    if(is_nan($number))
+      return $arg[0];
+    
+    if($number < -1 || $number > 1)
+      return new Value($obj->ecma, "Number", acos(8));
+    
+    if($number == 0)
+      return new Value($obj->ecma, "Number", 0);
+    
+    return new Value($obj->ecma, "Number", asin($number));
   }
 }
 
