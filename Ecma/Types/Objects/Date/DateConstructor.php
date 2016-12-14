249da3e4d150bc2a888a3ddcd86b4393e396841d
Ronnie <rix17172@gmail.com>
2016-12-14 10:15:20 +0100

    Update DateConstructor.php

--- Ecma/Types/Objects/Date/DateConstructor.php
+++ Ecma/Types/Objects/Date/DateConstructor.php
@@ -1,1 +1,14 @@
+<?php
+namespace Ecma\Types\Objects\Date\DateConstructor;
 
+use Ecma\Types\Objects\Constructor\Constructor;
+
+class DateConstructor extends HeadObject implements Construtor, Call{
+  public function Construct(array $arg) : Value{
+    
+  }
+  
+  public function Call(Value $obj, array $arg){
+    return new Value($obj->ecma, "String", $this->Construct([])->ToString());
+  }
+}
