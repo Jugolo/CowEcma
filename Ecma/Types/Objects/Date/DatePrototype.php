d4be469776f5f04a54dc593a7e65d435b2333bec
Ronnie <rix17172@gmail.com>
2016-12-14 10:27:05 +0100

    Update DatePrototype.php

--- Ecma/Types/Objects/Date/DatePrototype.php
+++ Ecma/Types/Objects/Date/DatePrototype.php
@@ -1,1 +1,25 @@
+<?php
+namespace Ecma\Types\Objects\Date\DataPrototype;
 
+use Ecma\Types\Objects\HeadObject;
+use Ecma\Types\Objects\Call\Call;
+use Ecma\Types\Objects\Property\Property;
+use Ecma\Types\Objects\Date\DateConstructor\DateConstructor;
+use Ecma\Types\Value\Value;
+use Ecma\Ecma\Ecma;
+
+class DatePrototype extends HeadObject{
+  protected $ecma;
+  
+  public function __construct(Ecma $ecma, DateConstructor $constructor){
+    $this->ecma = $ecma;
+    $this->Put("constructor", new Property(new Value($ecma, "Object", $constructor)));
+    $this->Put("toString",    new Property(new Value($ecma, "Object", new DateToString())));
+  }
+}
+
+class DateToString extends HeadObject implements Call{
+  public function Call(Value $obj, array $arg) : Value{
+    $o = $obj->ToObject();
+  }
+}
