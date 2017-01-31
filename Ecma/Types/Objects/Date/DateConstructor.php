0714fa4e0dcaa847461eb79dd52e7b3cdf53056f
Ronnie <rix17172@gmail.com>
2017-01-31 10:22:54 +0100

    Update DateConstructor.php

--- Ecma/Types/Objects/Date/DateConstructor.php
+++ Ecma/Types/Objects/Date/DateConstructor.php
@@ -1,9 +1,21 @@
 <?php
 namespace Ecma\Types\Objects\Date\DateConstructor;
 
+use Ecma\Ecma\Ecma;
+use Ecma\Types\Objects\Call\Call;
 use Ecma\Types\Objects\Constructor\Constructor;
+use Ecma\Types\Objects\Property\Property;
+use Ecma\Types\Objects\Date\DatePrototype\DatePrototype;
 
 class DateConstructor extends HeadObject implements Construtor, Call{
+  protected $ecma;
+  
+  public function __construct(Ecma $ecma){
+    $this->Prototype = $ecma->_function;
+    $this->Put("prototype", new Property(new Value($ecma, "Object", new DatePrototype($ecma, $this)), ["DontEnum", "DontDelete", "ReadOnly"]));
+    
+  }
+  
   public function Construct(array $arg) : Value{
     
   }
