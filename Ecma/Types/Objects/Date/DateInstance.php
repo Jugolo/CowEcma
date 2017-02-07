c064c97567f4a3dc0afbf31e5bd6695b82d72adb
Ronnie <rix17172@gmail.com>
2017-02-07 10:18:39 +0100

    Update DateInstance.php

--- Ecma/Types/Objects/Date/DateInstance.php
+++ Ecma/Types/Objects/Date/DateInstance.php
@@ -4,5 +4,9 @@ namespace Ecma\Types\Objects\Date\DateInstance;
 use Ecma\Types\Objects\HeadObject\HeadObject;
 
 class DateInstance extends HeadObject{
-
+  public function __construct(DateConstructor $date, int $time){
+    $this->Prototype = $date->Get("prototype")->GetValue()->ToObject();
+    $this->Class = "Date";
+    $this->Value = $time;
+  }
 }
