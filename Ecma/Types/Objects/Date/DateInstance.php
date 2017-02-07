b952606d72a53a9846f22ed2d8a5d59fe09f71fc
Ronnie <rix17172@gmail.com>
2017-02-07 10:19:57 +0100

    Update DateInstance.php

--- Ecma/Types/Objects/Date/DateInstance.php
+++ Ecma/Types/Objects/Date/DateInstance.php
@@ -2,6 +2,7 @@
 namespace Ecma\Types\Objects\Date\DateInstance;
 
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use Ecma\Types\Objects\Date\DateConstructor\DateConstructor;
 
 class DateInstance extends HeadObject{
   public function __construct(DateConstructor $date, int $time){
