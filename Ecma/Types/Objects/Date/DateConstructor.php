f1b4f6ca0a354a19ac1f850829d1cb0374bcd63f
Ronnie <rix17172@gmail.com>
2017-02-07 09:35:13 +0100

    Update DateConstructor.php

--- Ecma/Types/Objects/Date/DateConstructor.php
+++ Ecma/Types/Objects/Date/DateConstructor.php
@@ -8,6 +8,14 @@ use Ecma\Types\Objects\Constructor\Constructor;
 use Ecma\Types\Objects\Property\Property;
 use Ecma\Types\Objects\Date\DatePrototype\DatePrototype;
 use Ecma\Types\Objects\HeadObject\HeadObject;
+use function Ecma\Types\Objects\Date\DatePrototype\EcmaTime;
+use function Ecma\Types\Objects\Date\DatePrototype\YearFromTime;
+use function Ecma\Types\Objects\Date\DatePrototype\MonthFromTime;
+use function Ecma\Types\Objects\Date\DatePrototype\DateFromTime;
+use function Ecma\Types\Objects\Date\DatePrototype\HourFromTime;
+use function Ecma\Types\Objects\Date\DatePrototype\MinFromTime;
+use function Ecma\Types\Objects\Date\DatePrototype\SecFromTime;
+use function Ecma\Types\Objects\Date\DatePrototype\msFromTime;
 
 class DateConstructor extends HeadObject implements Construtor, Call{
   protected $ecma;
@@ -16,6 +24,7 @@ class DateConstructor extends HeadObject implements Construtor, Call{
     $this->Prototype = $ecma->_function;
     $this->Put("prototype", new Property(new Value($ecma, "Object", new DatePrototype($ecma, $this)), ["DontEnum", "DontDelete", "ReadOnly"]));
     $this->Put("parse",     new Property(new Value($ecma, "Object", new DateParse($this))));
+    $this->Put("UTC",       new Property(new Value($ecma, "Object", new DateUTC())));
   }
   
   public function Construct(array $arg) : Value{
@@ -24,6 +33,54 @@ class DateConstructor extends HeadObject implements Construtor, Call{
   
   public function Call(Value $obj, array $arg){
     return new Value($obj->ecma, "String", $this->Construct([])->ToString());
+  }
+}
+
+class DateUTC extends HeadObject implements Call{
+  public function Call(Value $obj, array $arg){
+    $size = count($arg);
+    $time = EcmaTime();
+    if($size == 0){
+      $year   =  YearFromTime($time);
+      $month  = MonthFromTime($time);
+      $date   = DateFromTime($time);
+      $hours  = HourFromTime($time);
+      $minut  = MinFromTime($time);
+      $second = SecFromTime($time);
+      $ms     = msFromTime($time);
+    }elseif($size == 1){
+      $year   = $arg[0]->ToNumber();
+      $month  = MonthFromTime($time);
+      $date   = DateFromTime($time);
+      $hours  = HourFromTime($time);
+      $minut  = MinFromTime($time);
+      $second = SecFromTime($time);
+      $ms     = msFromTime($time);
+    }elseif($size == 2){
+      $year   = $arg[0]->ToNumber();
+      $month  = $arg[1]->ToNumber();
+      $date   = DateFromTime($time);
+      $hours  = HourFromTime($time);
+      $minut  = MinFromTime($time);
+      $second = SecFromTime($time);
+      $ms     = msFromTime($time);
+    }elseif($size == 3){
+      $year   = $arg[0]->ToNumber();
+      $month  = $arg[1]->ToNumber();
+      $date   = $arg[2]->ToNumber();
+      $hours  = HourFromTime($time);
+      $minut  = MinFromTime($time);
+      $second = SecFromTime($time);
+      $ms     = msFromTime($time);
+    }elseif($size == 4){
+      $year   = $arg[0]->ToNumber();
+      $month  = $arg[1]->ToNumber();
+      $date   = $arg[2]->ToNumber();
+      $hours  = $arg[3]->ToNumber();
+      $minut  = MinFromTime($time);
+      $second = SecFromTime($time);
+      $ms     = msFromTime($time);
+    }
   }
 }
 
